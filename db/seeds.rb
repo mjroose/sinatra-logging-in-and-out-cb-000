5e5dbe34732bc891f350697b1e5e889dc3a7ef4a
Michael Roose <mjroose@gmail.com>
2018-10-28 21:02:43 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require_relative './../app/models/user.rb'
+
 user1 = User.create(:username => "skittles123", :password => "iluvskittles", :balance => 1000)
 
 user2 = User.create(:username => "flatiron4lyfe", :password => "Rubie!", :balance => 500)
